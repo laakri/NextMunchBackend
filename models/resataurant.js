58455e22e5e426e8cb891ba980bb97280ec80b8e
Jelassi Saif <68915309+laakri@users.noreply.github.com>
2023-12-13 13:42:55 +0100

    change the resto schema

--- models/resataurant.js
+++ models/resataurant.js
@@ -1,21 +1,18 @@
 const mongoose = require("mongoose");
 const User = require("./user");
 
-
-const restaurantSchema = mongoose.Schema({
-
-  ownerId: { type: mongoose.Schema.Types.ObjectId, ref: User },  
-  bannerImg: { type: String  , required: true},
-  mainImg: { type: String  , required: true},
-  nameR: { type: String  , required: true},
-  descriptionR: { type: String  , required: true},
-  location: { type: String  , required: true},
-  contact: { type: String  , required: true},
-  
-}
-,{ timestamps: true }
+const restaurantSchema = mongoose.Schema(
+  {
+    ownerId: { type: mongoose.Schema.Types.ObjectId, ref: User },
+    cin: { type: Number, required: true },
+    bannerImg: { type: String },
+    mainImg: { type: String },
+    nameR: { type: String, required: true },
+    descriptionR: { type: String },
+    location: { type: String, required: true },
+    contact: { type: String, required: true },
+  },
+  { timestamps: true }
 );
-
-
 
 module.exports = mongoose.model("Restaurant", restaurantSchema);

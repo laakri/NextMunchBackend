f0ee62d83d26f86b19f4b89d6c515964a75c0921
Jelassi Saif <68915309+laakri@users.noreply.github.com>
2023-12-13 10:57:13 +0100

    12/132023
    Laakri

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const userRoutes = require("./routes/user");
+const restaurantRoutes = require("./routes/restaurant");
 
 const app = express();
 
@@ -41,5 +42,6 @@ app.use((req, res, next) => {
 });
 
 app.use("/api/users", userRoutes);
+app.use("/api/restos", restaurantRoutes);
 
 module.exports = app;

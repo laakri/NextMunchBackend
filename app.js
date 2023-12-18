f6096adcb7ba740827514182fce86d4e658bbc69
Jelassi Saif <68915309+laakri@users.noreply.github.com>
2023-12-18 19:49:17 +0100

    add the update resto  methode

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ mongoose
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/file-folder", express.static(path.join("file-folder")));
+app.use("/uploads", express.static(path.join("uploads")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
--- routes/restaurant.js
+++ routes/restaurant.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const Restaurant = require("../models/resataurant");
 const router = express.Router();
+const multer = require("multer");
+const { promisify } = require("util");
+const fs = require("fs");
+const unlinkAsync = promisify(fs.unlink);
+const path = require("path");
 
 router.post("/restaurants", async (req, res) => {
   try {
@@ -31,12 +36,10 @@ router.post("/restaurants", async (req, res) => {
     const savedRestaurant = await newRestaurant.save();
 
     // Return the created restaurant's ID in the response
-    res
-      .status(201)
-      .json({
-        message: "Restaurant saved successfully",
-        restaurantId: savedRestaurant._id,
-      });
+    res.status(201).json({
+      message: "Restaurant saved successfully",
+      restaurantId: savedRestaurant._id,
+    });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -54,4 +57,63 @@ router.get("/restaurant/:id", async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 });
+/****************  Update Restaurant ***************** */
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/restaurant-images");
+  },
+  filename: function (req, file, cb) {
+    const ext = path.extname(file.originalname);
+    cb(null, Date.now() + ext);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+router.patch(
+  "/UpdateRestaurant/:restaurantId",
+  upload.fields([
+    { name: "mainImg", maxCount: 1 },
+    { name: "bannerImg", maxCount: 1 },
+  ]),
+  async (req, res) => {
+    const { restaurantId } = req.params;
+    const updateParams = req.body;
+
+    try {
+      const url = req.protocol + "://" + req.get("host");
+
+      // Check if the restaurant exists
+      const existingRestaurant = await Restaurant.findById(restaurantId);
+      if (!existingRestaurant) {
+        return res.status(404).json({ error: "Restaurant not found" });
+      }
+
+      // Handle image upload for mainImg
+      if (req.files && req.files["mainImg"]) {
+        updateParams.mainImg =
+          url +
+          "/uploads/restaurant-images/" +
+          req.files["mainImg"][0].filename;
+      }
+
+      // Handle image upload for bannerImg
+      if (req.files && req.files["bannerImg"]) {
+        updateParams.bannerImg =
+          url +
+          "/uploads/restaurant-images/" +
+          req.files["bannerImg"][0].filename;
+      }
+
+      // Update the restaurant with the provided parameters
+      await Restaurant.findByIdAndUpdate(restaurantId, updateParams);
+
+      res.status(200).json({ message: "Restaurant updated successfully" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  }
+);
+
 module.exports = router;

fd4db9ae34b841df435fbf31c430d64a00f18db7
Jelassi Saif <68915309+laakri@users.noreply.github.com>
2023-12-21 23:11:05 +0100

    reviews

--- routes/reviews.js
+++ routes/reviews.js
@@ -25,5 +25,29 @@ router.post("/rating/:userId/:restoId", async (req, res) => {
       .json({ error: "Erreur lors de la soumission de l'évaluation." });
   }
 });
+router.get("/getrating/:restoId", async (req, res) => {
+  const restoId = req.params.restoId;
 
+  try {
+    const reviews = await Review.find({ restoId });
+
+    if (reviews.length > 0) {
+      // Calculate the average rating
+      const averageRating =
+        reviews.reduce((acc, review) => acc + review.rating, 0) /
+        reviews.length;
+
+      res.json({ reviews, averageRating });
+    } else {
+      res.json({
+        message: "Aucune évaluation pour cet utilisateur et ce restaurant.",
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ error: "Erreur lors de la récupération des évaluations." });
+  }
+});
 module.exports = router;

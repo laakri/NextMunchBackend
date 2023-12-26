4664fa66fdfba2cf773342eeda49877dde419cd6
rouass <98843428+rouass@users.noreply.github.com>
2023-12-26 21:20:18 +0100

    totalForEachvoteReview

--- routes/reviews.js
+++ routes/reviews.js
@@ -26,30 +26,37 @@ router.post("/rating/:userId/:restoId", async (req, res) => {
   }
 });
 
-
 router.get("/getrating/:restoId", async (req, res) => {
-    const restoId = req.params.restoId;
-  
-    try {
-      const reviews = await Review.find({ restoId }).populate("userId", "name");
-  
-      if (reviews.length > 0) {
-        // Calculate the average rating
-        const averageRating =
-          reviews.reduce((acc, review) => acc + review.rating, 0) /
-          reviews.length;
-  
-        res.json({ reviews, averageRating });
-      } else {
-        res.json({
-          message: "Aucune évaluation pour cet utilisateur et ce restaurant.",
-        });
+  const restoId = req.params.restoId;
+
+  try {
+    const reviews = await Review.find({ restoId }).populate("userId", "name");
+
+    if (reviews.length > 0) {
+      // Calculate the average rating
+      const averageRating =
+        reviews.reduce((acc, review) => acc + review.rating, 0) /
+        reviews.length;
+
+      // Count the votes for each star rating
+      const ratingsCount = {};
+      for (let i = 1; i <= 5; i++) {
+        const count = reviews.filter((review) => review.rating === i).length;
+        ratingsCount[i] = count;
       }
-    } catch (error) {
-      console.error(error);
-      res
-        .status(500)
-        .json({ error: "Erreur lors de la récupération des évaluations." });
+
+      res.json({ reviews, averageRating, ratingsCount });
+    } else {
+      res.json({
+        message: "Aucune évaluation pour cet utilisateur et ce restaurant.",
+      });
     }
-  });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ error: "Erreur lors de la récupération des évaluations." });
+  }
+});
+
 module.exports = router;
